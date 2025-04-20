1a4f052064f8a88066216d4318aeeb6e1cd85939
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-20 19:06:25 +0500

    Add function to get computer choice
    - Add function to chose from rock, paper or scissors
    - Use Math.random function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let random = Math.random();
+
+    if (random < 0.3) {
+        return "rock";
+    } else if (random < 0.6) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
