e514f00496760425371a96ad568e13a566c3bd51
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-20 20:01:06 +0500

    Add function to play the round
    - Add `playRound()` function to compare human vs computer choices
    - Determines winner based on game rules
    - Logs round results and updates scores

--- script.js
+++ script.js
@@ -12,5 +12,27 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let human = prompt("Chose from 'rock', 'paper' or 'scissors'");
-    return human;
+    return human.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Its a tie!");
+    } else if (
+        humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
